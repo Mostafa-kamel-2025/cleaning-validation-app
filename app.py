11e25891a9a3af1d3177e36c6888c49c04ddfdf8
gjyanalysis <gjy.analysis@gmail.com>
2025-06-15 17:26:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,27 +9,21 @@ st.markdown(
     """
     <h1 style="
         text-align: center;
-        background: linear-gradient(90deg, #0d1975 200%, #1976d2 30%, #00b8d4 70%, #43e97b 10%);
+        background: linear-gradient(90deg, #47c6ff 10%, #60aaff 30%, #a084ee 60%, #ff6495 80%, #ff3838 100%);
         -webkit-background-clip: text;
         -webkit-text-fill-color: transparent;
         background-clip: text;
         color: transparent;
         font-weight: bold;
         letter-spacing:2px;
-        margin-bottom: 8px;
-    ">
+        margin-bottom: 10px;
+        font-size: 3em;
+        ">
         MACO Calculation App By Gopal Mandloi
     </h1>
-    <div style='text-align: center;'>
-        <img src='https://images.unsplash.com/photo-1515378791036-0648a3ef77b2?auto=format&fit=facearea&w=800&h=200&q=80' width='180' style='margin:10px'/>
-        <img src='https://images.unsplash.com/photo-1505751172876-fa1923c5c528?auto=format&fit=facearea&w=800&h=200&q=80' width='180' style='margin:10px'/>
-        <img src='https://images.unsplash.com/photo-1506744038136-46273834b3fb?auto=format&fit=facearea&w=800&h=200&q=80' width='180' style='margin:10px'/>
-    </div>
-    <h4 style='text-align: center; color: #444;'>A one-stop solution for MACO, Swab Limit, and Rinse Limit calculations in cleaning validation</h4>
     """,
     unsafe_allow_html=True
 )
-
 st.markdown("### View Final Results")
 
 # --- Data upload section ---
